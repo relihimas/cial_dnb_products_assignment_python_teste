d195cf8bb7f5d1c744849004ef22fd9a09e536eb
Rachid Elihimas <Rachid.elihimas@gmail.com>
2024-08-29 14:47:11 -0300

    ajuste sh

--- app/constants.py
+++ app/constants.py
@@ -1,9 +1,9 @@
 key_polygon='bmN7i7CrzrpKqFvgbB1fEaztCwZKSUjJ'
-dbname=''
-user=''
-password=''
-host=''
-port=''
+dbname='stock_db'
+user='stock_user'
+password='password'
+host='localhost'
+port='5432'
 insert_query="""INSERT INTO stock (stock, amount, created_on, updated_at) VALUES (%s, %s, %s, NOW() AT TIME ZONE 'America/Sao_Paulo')"""
 update_query="""UPDATE stock SET amount = %s, updated_at = NOW() AT TIME ZONE 'America/Sao_Paulo' WHERE stock = %s"""
 select_query="""SELECT amount FROM stock WHERE stock = %s""" 
